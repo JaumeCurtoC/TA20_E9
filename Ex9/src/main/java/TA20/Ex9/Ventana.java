0824146eb3181513a7b16f2a49053dbf22d8a610
C4rim-cibersegurata <114733605+C4rim-cibersegurata@users.noreply.github.com>
2022-10-27 13:43:56 +0200

    Update Ventana.java

--- Ex9/src/main/java/TA20/Ex9/Ventana.java
+++ Ex9/src/main/java/TA20/Ex9/Ventana.java
@@ -33,60 +33,80 @@ public class Ventana extends JFrame {
 	 */
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 552, 391);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(4, 4, -3, -3));
-		
-		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton);
-		
-		JToggleButton tglbtnNewToggleButton_2 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_2);
-		
-		JToggleButton tglbtnNewToggleButton_3 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_3);
-		
-		JToggleButton tglbtnNewToggleButton_4 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_4);
-		
-		JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_1);
-		
-		JToggleButton tglbtnNewToggleButton_8 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_8);
-		
-		JToggleButton tglbtnNewToggleButton_6 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_6);
+		contentPane.setLayout(new GridLayout(4, 4, 0, 0));
 		
 		JToggleButton tglbtnNewToggleButton_5 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_5.setBackground(new Color(0, 255, 255));
+		tglbtnNewToggleButton_5.setSelected(true);
 		contentPane.add(tglbtnNewToggleButton_5);
 		
-		JToggleButton tglbtnNewToggleButton_9 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9);
+		JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_1.setBackground(new Color(0, 255, 255));
+		tglbtnNewToggleButton_1.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_1);
+		
+		JToggleButton tglbtnNewToggleButton_6 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_6.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_6);
+		
+		JToggleButton tglbtnNewToggleButton_4 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_4.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_4);
+		
+		JToggleButton tglbtnNewToggleButton_3 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_3.setSelected(true);
+		tglbtnNewToggleButton_3.setForeground(new Color(0, 0, 0));
+		tglbtnNewToggleButton_3.setBackground(new Color(255, 255, 255));
+		contentPane.add(tglbtnNewToggleButton_3);
+		
+		JToggleButton tglbtnNewToggleButton_2 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_2.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_2);
+		
+		JToggleButton tglbtnNewToggleButton_8 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_8.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_8);
 		
 		JToggleButton tglbtnNewToggleButton_7 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_7.setSelected(true);
 		contentPane.add(tglbtnNewToggleButton_7);
 		
-		JToggleButton tglbtnNewToggleButton_9_1 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_1);
+		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton);
 		
-		JToggleButton tglbtnNewToggleButton_9_3 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_3);
+		JToggleButton tglbtnNewToggleButton_9 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_9.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_9);
 		
-		JToggleButton tglbtnNewToggleButton_9_2 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_2);
+		JToggleButton tglbtnNewToggleButton_10 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_10.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_10);
 		
-		JToggleButton tglbtnNewToggleButton_9_4 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_4);
+		JToggleButton tglbtnNewToggleButton_11 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_11.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_11);
 		
-		JToggleButton tglbtnNewToggleButton_9_7 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_7);
+		JToggleButton tglbtnNewToggleButton_12 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_12.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_12);
 		
-		JToggleButton tglbtnNewToggleButton_9_5 = new JToggleButton("New toggle button");
-		contentPane.add(tglbtnNewToggleButton_9_5);
+		JToggleButton tglbtnNewToggleButton_13 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_13.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_13);
+		
+		JToggleButton tglbtnNewToggleButton_14 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_14.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_14);
+		
+		JToggleButton tglbtnNewToggleButton_15 = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton_15.setSelected(true);
+		contentPane.add(tglbtnNewToggleButton_15);
 	}
 
 }
